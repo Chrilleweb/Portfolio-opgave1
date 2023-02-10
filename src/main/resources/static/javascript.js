1078c63d654b6fb9fad430ff1f1e375a5744f4b5
ChristianMN <christian.munknissen@gmail.com>
2023-02-10 10:49:16 +0100

    scroll down med js

---
+++ src/main/resources/static/javascript.js
@@ -0,0 +1,6 @@
+function scrollToTarget(event) {
+    event.preventDefault();
+    const targetId = event.target.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    targetElement.scrollIntoView({ behavior: "smooth" });
+}
